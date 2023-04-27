3970237945919ea06c497511deb2c786d8f06337
Michael Cheers <michaelcheers@users.noreply.github.com>
2023-04-27 09:22:38 +0100

    Add project files.

---
+++ AsdaOrdering/Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI_API.Chat;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Use gpt-3.5-turbo
+OpenAI_API.OpenAIAPI api = new OpenAI_API.OpenAIAPI(new OpenAI_API.APIAuthentication
+{
+    ApiKey = File.ReadAllText("openai.key")
+});
